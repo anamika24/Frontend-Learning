1f143a1e9dbd64384d21708ad139f156ba7d0945
Anamika Makkar <anni24.makkar@gmail.com>
2020-06-05 12:06:52 +0530

    addition of utils

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('postman-request')
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=e4249aa6d245d138e693230d05fb8e09&query=28.8955,76.6066&units=f'
 
@@ -27,26 +28,8 @@ const request = require('postman-request')
 //     }
 // })
 
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1IjoiYW5hbWlrYTI0IiwiYSI6ImNrYXc5ZjZtNjAzNG4ycmxpOTQwOHB2aTQifQ.bYpRtMhwhOkdoS6srpQrtA&limit=1' 
 
-    request({ url: url, json: true }, (error, response) => {
-            if (error) {
-                callback("Unable to connect to Location services!", undefined)
-            } else if (response.body.features.length === 0) {
-                callback("No location found. Try another search", undefined)
-            } else {
-                callback(undefined, 
-                    {
-                    latitude: response.body.features[0].center[1],
-                    longitude: response.body.features[0].center[0],
-                    location: response.body.features[0].place_name
-                }) 
-            }
-        })
-}
-
-geocode('Philadelphia', (error, data) => {
+geocode('Chandigarh', (error, data) => {
     console.log('Error: ', error)  
     console.log('Data: ' , data)
 })
