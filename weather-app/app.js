974ec56911218a7e969960991946b1dc82c3858f
Anamika Makkar <anni24.makkar@gmail.com>
2020-06-05 14:23:12 +0530

    Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,15 +2,12 @@ const request = require('postman-request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
-
-
 geocode('Chandigarh', (error, data) => {
     console.log('Error: ', error)  
     console.log('Data: ' , data)
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error: ', error)
+        console.log('Data: ', data)
+    })
 })
 
-forecast(30.73528, 76.79111, (error, data) => {
-    console.log('Error: ', error)
-    console.log('Data: ', data)
-})
