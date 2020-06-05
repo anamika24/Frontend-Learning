a91fd45f01866c5eb6de2c28e8bea9449f67a8b1
Anamika Makkar <anni24.makkar@gmail.com>
2020-06-05 15:11:03 +0530

    Optimized Callback chaining with Input taken by user through terminal

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,17 +2,25 @@ const request = require('postman-request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Chandigarh', (error, data) => {
-     if (error) {
-        return console.log(error)
-    }
-    
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
-       if (error) {
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
            return console.log(error)
        }
-       console.log(data.location)
-       console.log(forecastData)
-    })
-})
+       
+       forecast(data.latitude, data.longitude, (error, forecastData) => {
+          if (error) {
+              return console.log(error)
+          }
+          console.log(data.location)
+          console.log(forecastData)
+       })
+   })
+}
+
+
 
