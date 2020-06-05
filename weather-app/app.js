1059dad50aefed5e5f5ff745a5eac10e2317aabb
Anamika Makkar <anni24.makkar@gmail.com>
2020-06-05 14:30:45 +0530

    Optimized Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,11 +3,16 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('Chandigarh', (error, data) => {
-    console.log('Error: ', error)  
-    console.log('Data: ' , data)
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error: ', error)
-        console.log('Data: ', data)
+     if (error) {
+        return console.log(error)
+    }
+    
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+       if (error) {
+           return console.log(error)
+       }
+       console.log(data.location)
+       console.log(forecastData)
     })
 })
 
