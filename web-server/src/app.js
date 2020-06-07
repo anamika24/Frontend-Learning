33b33bfbc0fe025c908c09cad95790ed2c95e3fb
Anamika Makkar <anni24.makkar@gmail.com>
2020-06-07 07:13:46 +0530

    Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,23 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Anamika',
-        age: 24
-    })
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h2>About</h2>')
-})
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'Anamika',
+//         age: 24
+//     })
+// })
+// app.get('/about', (req, res) => {
+//     res.send('<h2>About</h2>')
+// })
 
 app.get('/weather', (req, res) => {
     res.send({
