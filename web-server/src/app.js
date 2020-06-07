1b038836b090983a22984d2b4d035dd0e783ba33
Anamika Makkar <anni24.makkar@gmail.com>
2020-06-07 08:55:54 +0530

    Dynamic pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,16 +4,33 @@ const express = require('express')
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Anamika'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Anamika'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        helptext: 'This is some helpful application.'
+    })
+})
 
 // app.get('', (req, res) => {
 //     res.send('<h1>Weather</h1>')
-// })
-// app.get('/help', (req, res) => {
-//     res.send({
-//         name: 'Anamika',
-//         age: 24
-//     })
 // })
 // app.get('/about', (req, res) => {
 //     res.send('<h2>About</h2>')
