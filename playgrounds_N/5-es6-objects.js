1a9a1ab73bf974be01e7547a0325a4e82fcd829b
anamika24 <anni24.makkar@gmail.com>
2020-08-07 22:12:17 +0530

    web-server-v1_NodeProject

--- playgrounds_N/5-es6-objects.js
+++ playgrounds_N/5-es6-objects.js
@@ -23,8 +23,9 @@ const product = {
 // console.log(productLabel)
 // console.log(rating)
 
-const transaction = (type, {label, stock}) => {
+const transaction = (type, {label, stock =0} = {}) => {
     console.log(type, label, stock)
 }
 
-transaction('order', product)
+ transaction('order', product)
+//transaction('order')
